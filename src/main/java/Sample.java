1e63c851fb5a620680fc19a1ae626c3f1591fc50
nitesh281189 <Nagrawal512@gmail.com>
2023-06-10 11:36:41 +0530

    added initial dependencies for framework

--- src/main/java/Sample.java
+++ src/main/java/Sample.java
@@ -3,6 +3,8 @@ public class Sample {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
-    public void m123(){}
+    public void m1234(){}
+
+    public void m2(){}
 
 }
